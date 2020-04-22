51f1a3724872fbef47fe072112ba03f84202c995
DmitryMI <wet75787@tqosi.com>
2020-04-22 15:48:42 +0300

    EntityType created

---
+++ TileEngineSfmlCs/TileEngineSfmlCs/TileEngine/TypeManagement/EntityTypes/AssemblyEntityType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TileEngineSfmlCs.TileEngine.TileObjects;
+
+namespace TileEngineSfmlCs.TileEngine.TypeManagement.EntityTypes
+{
+    public class AssemblyEntityType : EntityType
+    {
+        public override TileObject Activate()
+        {
+            return (TileObject) Activator.CreateInstance(BaseType);
+        }
+    }
+}
---
+++ TileEngineSfmlCs/TileEngineSfmlCs/TileEngine/TypeManagement/EntityTypes/EntityType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TileEngineSfmlCs.TileEngine.TileObjects;
+
+namespace TileEngineSfmlCs.TileEngine.TypeManagement.EntityTypes
+{
+    public abstract class EntityType
+    {
+        public string Name { get; set; }
+
+        public Type BaseType { get; set; }
+
+        public abstract TileObject Activate();
+    }
+}
